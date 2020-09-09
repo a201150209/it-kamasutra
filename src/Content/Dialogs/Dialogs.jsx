d7b1c362b7c8ab9a8498d8f05fecd246c5d87425
Dmitriy <a20150209@gmail.com>
2020-09-09 13:53:20 +0300

    Добавил пропсы в диалоги

--- src/Content/Dialogs/Dialogs.jsx
+++ src/Content/Dialogs/Dialogs.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Dialog from "./Dialog/Dialog";
 
 const Dialogs = () => {
-  return <div>Это мои сообщения</div>;
+  return (
+    <div>
+      <p>Это мои сообщения</p>
+      <Dialog message="Диалог с Ваней" />
+      <Dialog message="Диалог с Мишей" />
+    </div>
+  );
 };
 
 export default Dialogs;
